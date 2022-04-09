d21eb66b0f68c7fd616dc33727b697fbcd789f6b
munevverozhan <merveozh@gmail.com>
2022-04-09 16:36:35 +0300

    Create Veritabani.java
    VERİTABANI PAKETİ OLUŞTURULDU. ADMİNS VE AGREEMENT TABLOLARI OLUSTURULDU

---
+++ src/java/veritabani/Veritabani.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package veritabani;
+
+/**
+ *
+ * @author munevver
+ */
+public class Veritabani {
+/*
+ create table admins 
+(
+admins_id integer primary key not null,
+	mail varchar,
+    passwords varchar
+);
+
+create table agreement 
+(
+agrement_id integer primary key not null,
+	agreement_date date,
+	
+	users_id integer references users(users_id)
+);
+
+
+*/
+}
