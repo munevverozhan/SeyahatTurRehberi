abc1e40f6456a5d1f94a84fe78b5feef96aa0542
Cansu Mecit <89852202+cansumct15@users.noreply.github.com>
2022-04-09 16:24:13 +0300

    ENTITY PAKETI OLUSTURULDU.CONSIDERATION VE HOLIDAYS SINIFI OLUŞTURULDU.

---
+++ src/java/entity/consideration.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author DELL
+ */
+public class consideration {
+    
+    private int consideration_id;
+
+    public consideration(int consideration_id) {
+        this.consideration_id = consideration_id;
+    }
+    
+    
+    
+    public int getConsideration_id() {
+        return consideration_id;
+    }
+
+    public void setConsideration_id(int consideration_id) {
+        this.consideration_id = consideration_id;
+    }
+    
+}
+
---
+++ src/java/entity/holidays.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author DELL
+ */
+public class holidays {
+     private int holidays_id;
+    private int type_id;
+    private Date holidays_date;
+
+    public holidays(int holidays_id, int type_id, Date holidays_date) {
+        this.holidays_id = holidays_id;
+        this.type_id = type_id;
+        this.holidays_date = holidays_date;
+    }
+    
+    
+
+    public int getHolidays_id() {
+        return holidays_id;
+    }
+
+    public void setHolidays_id(int holidays_id) {
+        this.holidays_id = holidays_id;
+    }
+
+    public int getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(int type_id) {
+        this.type_id = type_id;
+    }
+
+    public Date getHolidays_date() {
+        return holidays_date;
+    }
+
+    public void setHolidays_date(Date holidays_date) {
+        this.holidays_date = holidays_date;
+    }
+}
