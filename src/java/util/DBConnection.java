7aa2067d450d2919bd6847ec70e144b00b762a83
munevverozhan <merveozh@gmail.com>
2022-04-10 13:43:00 +0300

    veritabanı bağlantısı sağlandı.

--- src/java/util/DBConnection.java
+++ src/java/util/DBConnection.java
@@ -22,7 +22,7 @@ public abstract class DBConnection {
         Connection c=null;
         try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cms", "postgres", "12345");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Tur", "postgres", "12345");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
