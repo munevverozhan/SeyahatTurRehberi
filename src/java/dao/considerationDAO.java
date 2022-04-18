683f614b276645ed8b8e94b797534e80b491c86b
Ayşe Tunç <67433327+ayseceng@users.noreply.github.com>
2022-04-19 01:45:59 +0300

    hatalar düzeltildi

--- src/java/dao/considerationDAO.java
+++ src/java/dao/considerationDAO.java
@@ -4,9 +4,8 @@
  */
 package dao;
 
-
 import entity.consideration;
-import java.sql.Connection;
+import entity.users;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -15,16 +14,57 @@ import util.DBConnection;
 
 /**
  *
- * @author DEll
+ * @author aysetunc
  */
-public class considerationDAO extends DBConnection
-{
-     
+public class considerationDAO extends DBConnection {
+
+    private usersDAO usersDao;
+    public consideration findByID(int consideration_id) {
+        consideration c = null;
+        try {
+            Statement st = this.getConnection().createStatement();
+
+            String query = "select * from consideration where consideration_id=" + consideration_id;
+
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                c = (new consideration(rs.getInt("consideration_id"), rs.getString("context")));
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return c;
+
+    }
+
+    public void create(consideration c) {
+        try {
+            Statement st = this.getConnection().createStatement();
+
+            String query = "insert into consideration(context) values('" + c.getContext() + "')";
+            int r = st.executeUpdate(query);
+            ResultSet rs = st.executeQuery("select max(consideration_id) as mid from consideration");
+            rs.next();
+            int consideration_id = rs.getInt("mid");
+
+            for (users user : c.getUseries()) {
+                query = "insert into kullanici_deger(consideration_id,users_id) values(" + consideration_id + "," + user.getUsers_id() + ")";
+                st.executeUpdate(query);
+
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 
     public void delete(consideration c) {
         try {
-            Connection connect = this.connect();
-            Statement st = connect.createStatement();
+            Statement st = this.getConnection().createStatement();
+            st.executeUpdate("delete from kullanici_deger where consideration_id=" + c.getConsideration_id());
+
             String query = "delete from consideration where consideration_id=" + c.getConsideration_id();
             int r = st.executeUpdate(query);
 
@@ -33,17 +73,32 @@ public class considerationDAO extends DBConnection
         }
     }
 
-   
+    public void update(consideration c) {
+        try {
+            Statement st = this.getConnection().createStatement();
 
+            String query = "update consideration set context='" + c.getContext() + "' where consideration_id=" + c.getConsideration_id();
+            st.executeUpdate(query);
+            st.executeUpdate("delete from kullanici_deger where consideration_id=" + c.getConsideration_id());
+            for (users user : c.getUseries()) {
+                query = "insert into kullanici_deger(consideration_id,users_id) values(" + c.getConsideration_id() + "," + user.getUsers_id() + ")";
+                st.executeUpdate(query);
+
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
     public List<consideration> getConsiderationList() {
         List<consideration> considerationList = new ArrayList<>();
         try {
-            Connection connect = this.connect();
-            Statement st = connect.createStatement();
-            String query = "select * from consideration";
+            Statement st = this.getConnection().createStatement();
+
+            String query = "select * from consideration order by consideration_id asc";
             ResultSet rs = st.executeQuery(query);
             while (rs.next()) {
-                considerationList.add(new consideration(rs.getInt("consideration_id")));
+                considerationList.add(new consideration(rs.getInt("consideration_id"), rs.getString("context"),this.getConsiderationUser(rs.getInt("consideration_id"))));
 
             }
 
@@ -52,5 +107,32 @@ public class considerationDAO extends DBConnection
         }
         return considerationList;
     }
-}
+     public List<users> getConsiderationUser(int consideration_id) {
+        List<users> usersList = new ArrayList<>();
+        try {
+            Statement st = this.getConnection().createStatement();
 
+            String query = "select * from users where users_id in(select users_id from kullanici_deger where consideration_id=" + consideration_id + ")";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                usersList.add(new users(rs.getInt("users_id"), rs.getString("mail"), rs.getString("passwords"), rs.getString("first_name"), rs.getString("last_name")));
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return usersList;
+    }
+
+    public usersDAO getUsersDao() {
+         if (usersDao == null) {
+            this.usersDao = new usersDAO();
+        }
+        return usersDao;
+    }
+
+    public void setUsersDao(usersDAO usersDao) {
+        this.usersDao = usersDao;
+    }
+     
+}
--- src/java/dao/holidaysDAO.java
+++ src/java/dao/holidaysDAO.java
@@ -4,43 +4,68 @@
  */
 package dao;
 
-
 import entity.holidays;
-import java.sql.Connection;
+import entity.users;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import util.DBConnection;
 
-
-
-
-
 /**
  *
- * @author DELL
+ * @author aysetunc
  */
 public class holidaysDAO extends DBConnection {
-    
-    
-       public String createHolidays(holidays c) {
+
+    private usersDAO usersDao;
+
+    public holidays findByID(int holidays_id) {
+        holidays c = null;
         try {
-            Connection connect = this.connect();
-            Statement st = connect.createStatement();
-            String query = "insert into holidays(type_id,holidays_date) values('" + c.getHolidays_date() + "')";
+            Statement st = this.getConnection().createStatement();
+
+            String query = "select * from holidays where holidays_id=" + holidays_id;
+
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                c = (new holidays(rs.getInt("holidays_id"), rs.getString("holiday_name"), rs.getString("holiday_date")));
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return c;
+
+    }
+    public void createHolidays(holidays c) {
+        try {
+            Statement st = this.getConnection().createStatement();
+
+            String query = "insert into holidays(holiday_name,holiday_date) values('" + c.getHoliday_name() + "','" + c.getHoliday_date() + "')";
             int r = st.executeUpdate(query);
+            ResultSet rs = st.executeQuery("select max(holidays_id) as mid from holidays");
+            rs.next();
+            int holidays_id = rs.getInt("mid");
+
+            for (users user : c.getUseries()) {
+                query = "insert into kullanici_tatil(holidays_id,users_id) values(" + holidays_id + "," + user.getUsers_id() + ")";
+                st.executeUpdate(query);
+
+            }
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return "index";
+
     }
 
     public void delete(holidays c) {
         try {
-            Connection connect = this.connect();
-            Statement st = connect.createStatement();
+            Statement st = this.getConnection().createStatement();
+
+            st.executeUpdate("delete from kullanici_tatil where holidays_id=" + c.getHolidays_id());
+
             String query = "delete from holidays where holidays_id=" + c.getHolidays_id();
             int r = st.executeUpdate(query);
 
@@ -51,10 +76,16 @@ public class holidaysDAO extends DBConnection {
 
     public void update(holidays c) {
         try {
-            Connection connect = this.connect();
-            Statement st = connect.createStatement();
-            String query = "update holidays set type_id='" + c.getType_id() + "' where holidays_date=" + c.getHolidays_date();
-            int r = st.executeUpdate(query);
+            Statement st = this.getConnection().createStatement();
+
+            String query = "update holidays set holiday_name='" + c.getHoliday_name() + "', holiday_date='" + c.getHoliday_date() + "' where holidays_id=" + c.getHolidays_id();
+            st.executeUpdate(query);
+            st.executeUpdate("delete from kullanici_tatil where holidays_id=" + c.getHolidays_id());
+            for (users user : c.getUseries()) {
+                query = "insert into kullanici_tatil(holidays_id,users_id) values(" + c.getHolidays_id() + "," + user.getUsers_id() + ")";
+                st.executeUpdate(query);
+
+            }
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -64,12 +95,12 @@ public class holidaysDAO extends DBConnection {
     public List<holidays> getHolidaysList() {
         List<holidays> holidaysList = new ArrayList<>();
         try {
-            Connection connect = this.connect();
-            Statement st = connect.createStatement();
-            String query = "select * from holidays";
+            Statement st = this.getConnection().createStatement();
+
+            String query = "select * from holidays order by holidays_id asc ";
             ResultSet rs = st.executeQuery(query);
             while (rs.next()) {
-                holidaysList.add(new holidays(rs.getInt("type_id"), rs.getDate("holidays_date")));
+                holidaysList.add(new holidays(rs.getInt("holidays_id"), rs.getString("holiday_name"), rs.getString("holiday_date"), this.getHolidaysUseries(rs.getInt("holidays_id"))));
 
             }
 
@@ -78,7 +109,33 @@ public class holidaysDAO extends DBConnection {
         }
         return holidaysList;
     }
+
+    public List<users> getHolidaysUseries(int holidays_id) {
+        List<users> usersList = new ArrayList<>();
+        try {
+            Statement st = this.getConnection().createStatement();
+
+            String query = "select * from users where users_id in(select users_id from kullanici_tatil where holidays_id=" + holidays_id + ")";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                usersList.add(new users(rs.getInt("users_id"), rs.getString("mail"), rs.getString("passwords"), rs.getString("first_name"), rs.getString("last_name")));
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return usersList;
+    }
+
+    public usersDAO getUsersDao() {
+        if (usersDao == null) {
+            this.usersDao = new usersDAO();
+        }
+        return usersDao;
+    }
+
+    public void setUsersDao(usersDAO usersDao) {
+        this.usersDao = usersDao;
+    }
+
 }
-
-    
-
