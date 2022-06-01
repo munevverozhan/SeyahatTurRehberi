603bdc5cf3682b793570f8c1380561e1b1922dae
Ayşe Tunç <67433327+ayseceng@users.noreply.github.com>
2022-06-01 18:57:47 +0300

    Agreement için sayfalama ve ajax işlemleri oluşturuldu.

--- src/java/controller/agreementBean.java
+++ src/java/controller/agreementBean.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 /**
  *
- * @author munevver
+ * @author
  */
 @Named(value = "agreementBean")
 @SessionScoped
@@ -27,6 +27,53 @@ public class agreementBean implements Serializable {
     private agreement entity;
     private agreementDAO dao;
     private List<agreement> list;
+
+    private int page = 1;
+    private int pageSize = 4;
+    private int pageCount;
+
+    public void next() {
+        if (this.page == pageCount)// sonraki sayfaya geçecek metod.
+        {
+            this.page = 1;
+        } else {
+            this.page++;
+        }
+
+    }
+
+    public void previous() {// önceki sayfaya geçecek metod.
+        if (this.page == 1) {
+            this.page = this.getPageCount();
+        } else {
+            this.page--;
+        }
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageCount() { // kaç tane sayfamız olduğunu bulacak olan metod.
+        this.pageCount = (int) Math.ceil(this.getDao().count() / (double) pageSize);
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
 
     public agreement getEntity() {
         if (this.entity == null) {
@@ -51,7 +98,7 @@ public class agreementBean implements Serializable {
     }
 
     public List<agreement> getList() {
-        this.list = this.getDao().getAgreementList();
+        this.list = this.getDao().getAgreementList(page, pageSize);
         return list;
     }
 
@@ -63,7 +110,7 @@ public class agreementBean implements Serializable {
 
         this.getDao().create(entity);
         this.entity = new agreement();
-        
+
     }
 
     public void delete(agreement c) {
@@ -73,6 +120,10 @@ public class agreementBean implements Serializable {
     public void update() {
         this.getDao().update(this.entity);
         this.entity = new agreement();
+    }
+
+    public void clear() {
+        entity = new agreement();
     }
 
 }
--- src/java/dao/agreementDAO.java
+++ src/java/dao/agreementDAO.java
@@ -12,10 +12,11 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import util.DBConnection;
+import java.sql.PreparedStatement;
 
 /**
  *
- * @author munevver
+ * @author
  */
 public class agreementDAO extends DBConnection {
 
@@ -23,10 +24,12 @@ public class agreementDAO extends DBConnection {
 
     public void create(agreement c) {
         try {
-            Statement st = this.getConnection().createStatement();
 
-            String query = "insert into agreement(name,agreement_date,users_id) values('" + c.getName() + "','" + c.getAgreement_date() + "'," + c.getUsers().getUsers_id() + ")";
-            int r = st.executeUpdate(query);
+            PreparedStatement pst = this.getConnection().prepareStatement("insert into agreement(name,agreement_date,users_id) values (?,?,?)");
+            pst.setString(1, c.getName());
+            pst.setString(2, c.getAgreement_date());
+            pst.setInt(3, c.getUsers().getUsers_id());
+            pst.executeUpdate();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -36,10 +39,9 @@ public class agreementDAO extends DBConnection {
 
     public void delete(agreement c) {
         try {
-            Statement st = this.getConnection().createStatement();
-
-            String query = "delete from agreement where agreement_id=" + c.getAgreement_id();
-            int r = st.executeUpdate(query);
+            PreparedStatement pst = this.getConnection().prepareStatement("delete from agreement where agreement_id=?");
+            pst.setInt(1, c.getAgreement_id());
+            pst.executeUpdate();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -48,27 +50,37 @@ public class agreementDAO extends DBConnection {
 
     public void update(agreement c) {
         try {
-            Statement st = this.getConnection().createStatement();
-
-            String query = "update agreement set name='" + c.getName() + "',agreement_date='" + c.getAgreement_date() + "',users_id=" + c.getUsers().getUsers_id() + " where  agreement_id=" + c.getAgreement_id();
-            int r = st.executeUpdate(query);
+            PreparedStatement pst = this.getConnection().prepareStatement("update agreement set name=?,agreement_date=? ,users_id=? where  agreement_id=?");
+            pst.setString(1, c.getName());
+            pst.setString(2, c.getAgreement_date());
+            pst.setInt(3, c.getUsers().getUsers_id());
+            pst.setInt(4, c.getAgreement_id());
+            pst.executeUpdate();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public List<agreement> getAgreementList() {
+    public List<agreement> getAgreementList(int page, int pageSize) {
         List<agreement> agreementList = new ArrayList<>();
+        agreement obj;
+        int start = (page - 1) * pageSize;
+
         try {
+            PreparedStatement pst = this.getConnection().prepareStatement("select * from agreement order by agreement_id asc limit " + pageSize + " offset " + start);
+            ResultSet rs = pst.executeQuery();
+            //    Statement st = this.getConnection().createStatement();
 
-            Statement st = this.getConnection().createStatement();
-
-            String query = "select * from agreement order by agreement_id asc";
-            ResultSet rs = st.executeQuery(query);
+            //  String query = "select * from agreement order by agreement_id asc";
+            //ResultSet rs = st.executeQuery(query);
             while (rs.next()) {
-                users u = this.getUsersDao().findByID(rs.getInt("users_id"));
-                agreementList.add(new agreement(rs.getInt("agreement_id"), rs.getString("name"), rs.getString("agreement_date"), u));
+                obj = new agreement();
+                obj.setAgreement_id(rs.getInt("agreement_id"));
+                obj.setName(rs.getString("name"));
+                obj.setAgreement_date(rs.getString("agreement_date"));
+                obj.setUsers(this.getUsersDao().findByID(rs.getInt("users_id")));
+                agreementList.add(obj);
 
             }
 
@@ -76,6 +88,22 @@ public class agreementDAO extends DBConnection {
             System.out.println(e.getMessage());
         }
         return agreementList;
+    }
+
+    public int count() {
+        int count = 0;
+        try {
+            PreparedStatement pst = this.getConnection().prepareStatement("select count(agreement_id) as agreement_count from agreement  ");
+            ResultSet rs = pst.executeQuery();
+            //    Statement st = this.getConnection().createStatement();
+
+            //  String query = "select * from agreement order by agreement_id asc";
+            rs.next();
+            count = rs.getInt("agreement_count");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return count;
     }
 
     public usersDAO getUsersDao() {
